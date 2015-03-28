81f99d2341ec7d375175e8ae55d139e1a14a84d7
Jake Prather <JakeHP@Zoho.com>
2015-03-28 12:08:41 -0500

    Added inputs, classes for inputs, and placeholders.

--- src/js/app.js
+++ src/js/app.js
@@ -14,21 +14,60 @@ inputs.push({
     type: 'number',
     label: 'Monthly Expenses'
 });
+inputs.push({
+    id: 'safetyNetInput',
+    type: 'number',
+    label: 'Safety Net / Reserve'
+});
+inputs.push({
+    id: 'downPaymentInput',
+    type: 'number',
+    label: 'Down Payment'
+});
+inputs.push({
+    id: 'tradeInInput',
+    type: 'number',
+    label: 'Trade In'
+});
+inputs.push({
+    id: 'salesTaxInput',
+    type: 'number',
+    label: 'Sales Tax',
+    placeholder: '5.1'
+});
+inputs.push({
+    id: 'interestRateInput',
+    type: 'number',
+    label: 'Interest Rate (APR)',
+    placeholder: '2.9'
+});
+inputs.push({
+    id: 'termInput',
+    type: 'number',
+    label: 'Term (Months)',
+    placeholder: '72'
+});
 
 inputs.forEach(function(e){
    renderInput(e, 'content');
 });
 
 function renderInput(inputObj, parentElementId){
-    var div = document.createElement("div");
-    div.setAttribute("id", inputObj.id+ContainerKey);
-    document.getElementById(parentElementId).appendChild(div);
+    var div = createContainer(inputObj.id+ContainerKey, parentElementId);
     React.render(
         <InputComponent
             id={inputObj.id}
             type={inputObj.type}
             label={inputObj.label}
+            placeholder={inputObj.placeholder||''}
             />,
         div
     );
+}
+
+function createContainer(elementId, parentElementId){
+    var div = document.createElement("div");
+    div.setAttribute("id", elementId);
+    document.getElementById(parentElementId).appendChild(div);
+    return div;
 }
--- src/js/components/InputComponent/InputComponent.js
+++ src/js/components/InputComponent/InputComponent.js
@@ -4,8 +4,8 @@ var InputComponent = React.createClass({
     render: function() {
         return (
             <div>
-                <label htmlFor={this.props.id}>{this.props.label}</label>
-                <input id={this.props.id} type={this.props.type} name={this.props.id}/>
+                <label className="label label-default" htmlFor={this.props.id}>{this.props.label}</label>
+                <input className="form-control" id={this.props.id} type={this.props.type} name={this.props.id} placeholder={this.props.placeholder}/>
             </div>
         );
     }
