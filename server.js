ee4d7c67affe44311c28ce80217294e818095f41
Jake Prather <JakeHP@Zoho.com>
2015-04-07 20:27:15 -0500

    github pages compatiblity.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname)));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
