88327d6adbff53c1ad48c8655da9062db75125d3
Jake Prather <JakeHP@Zoho.com>
2015-04-12 21:23:48 -0500

    Init PercentOfNetIncome Component.

---
+++ src/js/components/ResultComponents/PercentOfNetIncomeComponent/PercentOfNetIncomeComponent.js
@@ -0,0 +1,46 @@
+var React = require('react');
+var InputStore = require('../../../stores/InputStore');
+var Validators = require('../../../util/Validators');
+var MonthlyCostInputDefinitions = require('../../../defines/MonthlyCostInputDefinitions');
+var ScenariosInputDefinitions = require('../../../defines/ScenariosInputDefinitions');
+var Calculations = require('../../../util/Calculations');
+
+var PercentOfNetIncomeComponent = React.createClass({
+    _onInputChange: function() {
+        this.setState({
+            inputs: InputStore.getAll()
+        });
+    },
+    componentDidMount: function() {
+        InputStore.addChangeListener(this._onInputChange);
+    },
+    componentWillUnmount: function() {
+        InputStore.removeChangeListener(this._onInputChange);
+    },
+    getInitialState: function() {
+        return {
+            inputs: InputStore.getAll()
+        };
+    },
+    render: function() {
+        var monthly = undefined;
+        if(
+            Validators.validateRequiredInputsExist(ScenariosInputDefinitions.get(), this.state.inputs) &&
+            Validators.validateRequiredInputsExist(MonthlyCostInputDefinitions.get(), this.state.inputs)
+        ){
+            monthly = Calculations.monthlyCost(
+                this.state.inputs.vehiclePrice,
+                this.state.inputs.downPayment,
+                this.state.inputs.tradeIn,
+                this.state.inputs.interestRate,
+                this.state.inputs.salesTax,
+                this.state.inputs.term
+            );
+        }
+        return (
+            <div></div>
+        );
+    }
+});
+
+module.exports = PercentOfNetIncomeComponent;
