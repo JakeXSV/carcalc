d852340f9bf217b1d91e5551e0d33911ea6c3b3f
Jake Prather <JakeHP@Zoho.com>
2015-04-16 20:39:17 -0500

    In incomebreakdown validate required inputs exist.

--- src/js/components/ResultListComponent/ResultComponents/IncomeBreakdownComponent.js
+++ src/js/components/ResultListComponent/ResultComponents/IncomeBreakdownComponent.js
@@ -2,9 +2,7 @@ var React = require('react');
 var Highcharts = require('react-highcharts');
 var InputStore = require('../../../stores/InputStore');
 var Validators = require('../../../util/Validators');
-var MonthlyCostInputDefinitions = require('../../../defines/MonthlyCostInputDefinitions');
-var ScenariosInputDefinitions = require('../../../defines/IncomeBreakdownInputDefinitions');
-var Calculations = require('../../../util/Calculations');
+var IncomeBreakdownInputDefinitions = require('../../../defines/IncomeBreakdownInputDefinitions');
 
 var IncomeBreakdownComponent = React.createClass({
     _onInputChange: function() {
@@ -25,75 +23,82 @@ var IncomeBreakdownComponent = React.createClass({
     },
     render: function() {
 
-        var income = this.state.inputs.monthlyIncome;
-        var expenses = this.state.inputs.monthlyExpenses;
+        var inputs = this.state.inputs;
 
-        if(income > 0){
-            var netIncome = income - expenses - this.props.monthlyCarCost;
-            var breakdowns = [
-                {
-                    label: 'Cash',
-                    value: (netIncome/income)*100
-                },
-                {
-                    label: 'Expenses',
-                    value: (expenses/income)*100
-                },
-                {
-                    label: 'Monthly Car Cost',
-                    value: (this.props.monthlyCarCost/income)*100
-                }
-            ];
+        if(Validators.validateRequiredInputsExist(IncomeBreakdownInputDefinitions.get(), inputs)){
 
-            var data = [];
-            breakdowns.forEach(function(e){
-                if(e.value > 0){
-                    data.push([e.label, e.value]);
-                }
-            });
-            var config = {
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: 0,
-                    plotShadow: false
-                },
-                title: {
-                    text: 'Income<br>Breakdown',
-                    align: 'center',
-                    verticalAlign: 'middle',
-                    y: 40
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        dataLabels: {
-                            enabled: true,
-                            distance: -50,
-                            style: {
-                                fontWeight: 'bold',
-                                color: 'white',
-                                textShadow: '0px 1px 2px black'
-                            }
-                        },
-                        startAngle: -90,
-                        endAngle: 90,
-                        center: ['50%', '75%']
+            var income = inputs.monthlyIncome;
+            var expenses = inputs.monthlyExpenses;
+
+            if(income > 0){
+
+                var netIncome = income - expenses - this.props.monthlyCarCost;
+                var breakdowns = [
+                    {
+                        label: 'Cash',
+                        value: (netIncome/income)*100
+                    },
+                    {
+                        label: 'Expenses',
+                        value: (expenses/income)*100
+                    },
+                    {
+                        label: 'Monthly Car Cost',
+                        value: (this.props.monthlyCarCost/income)*100
                     }
-                },
-                series: [{
-                    type: 'pie',
-                    name: 'Income Breakdown',
-                    innerSize: '50%',
-                    data: data
-                }]
-            };
-            return (
-                <div>
-                    <Highcharts config = {config}></Highcharts>
-                </div>
-            );
+                ];
+
+                var data = [];
+                breakdowns.forEach(function(e){
+                    if(e.value > 0){
+                        data.push([e.label, e.value]);
+                    }
+                });
+
+                var config = {
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: 0,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: 'Income<br>Breakdown',
+                        align: 'center',
+                        verticalAlign: 'middle',
+                        y: 40
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: true,
+                                distance: -50,
+                                style: {
+                                    fontWeight: 'bold',
+                                    color: 'white',
+                                    textShadow: '0px 1px 2px black'
+                                }
+                            },
+                            startAngle: -90,
+                            endAngle: 90,
+                            center: ['50%', '75%']
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'Income Breakdown',
+                        innerSize: '50%',
+                        data: data
+                    }]
+                };
+                return (
+                    <div>
+                        <Highcharts config = {config}></Highcharts>
+                    </div>
+                );
+            }
         }
         return (
             <div></div>
